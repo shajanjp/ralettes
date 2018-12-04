ca38533a26df5def55983485abb3443a33854524
Shajan Jacob <shajanjp@gmail.com>
2018-12-04 17:00:03 +0530

    adds text name and set color function

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+let colorSpace = document.getElementById('color-space');
+let colorName = document.getElementById('color-name')
+let colorPos = 0;
 function generateRandomNumber(min, max){
   return Math.floor(Math.random() * (max-min) + min);
 }
@@ -11,6 +14,22 @@ function shuffle(ordered){
   return shuffled;
 }
 
+function setColor(RGBColor){
+  colorSpace.style.background = `#${RGBColor[0]}`;
+  colorName.innerHTML = RGBColor[1];
+}
+
 let colorBucket = shuffle(colorList);
-let colorSpace = document.getElementById('color-space');
-colorSpace.style.background = `#${colorBucket[0][0]}`;
+
+setColor(colorBucket[colorPos])
+
+document.getElementsByTagName("body")[0]
+.addEventListener("click", function(){
+  setColor(colorBucket[colorPos++]);
+});
+
+document.addEventListener("keydown", function(event) {
+  if(event.which == 32){
+   setColor(colorBucket[colorPos++]);
+  }
+});
